09fd7586705eb8e45e25a5e75d271c038d089782
minsulee2 <minsu@comcom.ai>
2020-11-16 11:19:17 +0900

    Update v1

--- playground/mlp/mnist_mlp_v1.py
+++ playground/mlp/mnist_mlp_v1.py
@@ -9,8 +9,9 @@ from __future__ import print_function
 
 import keras
 import numpy as np
+import matplotlib.pyplot as plt
 from keras.datasets import mnist
-from keras.models import Sequential, load_model
+from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import RMSprop
 from keras.preprocessing import image
@@ -58,10 +59,20 @@ score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
 
+test = x_test[100].reshape(28, 28)
+plt.imshow(test)
+test = x_test[100].reshape(1, 784)
+result = model.predict(test)
+print(y_test[100])
+print(np.argmax(result))
+
 img_width, img_height = 28, 28
-test_image = image.load_img('./images/2_small.png', color_mode='grayscale', target_size=(img_width, img_height))
+test_image = image.load_img('./images/2.png', color_mode="grayscale", target_size=(img_width, img_height))
 test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis=0)
+test_image = test_image.astype('float32')
+test_image = test_image.reshape(28, 28)
+test_image = 255 - test_image
+test_image /= 255
 test_image = test_image.reshape(1, img_width * img_height)
 result = model.predict(test_image, batch_size=1)
-print(result)
+print(np.argmax(result))
